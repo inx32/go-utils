198935204b32a929e1ec29f810ada20124f7d132
infernox-dev <109545908+infernox-dev@users.noreply.github.com>
2025-07-23 01:38:58 +0300

    cmap: Don't use interfaces

--- cmap/cmap.go
+++ cmap/cmap.go
@@ -2,42 +2,29 @@ package cmap
 
 import "sync"
 
-// Thread-safe map
-type Cmap[K comparable, V any] interface {
-	Set(key K, value V)
-	SetFunc(key K, f func(value V, ex bool) V)
-	Remove(key K)
-	Get(key K) (value V, ex bool)
-	GetDefault(key K, def V) (value V)
-	Has(key K) (ex bool)
-	Keys() <-chan K
-	Values() <-chan V
-	Range() <-chan CmapField[K, V]
-}
-
-type cmapImpl[K comparable, V any] struct {
+type Cmap[K comparable, V any] struct {
 	m  map[K]V
 	mu sync.RWMutex
 }
 
-func (t *cmapImpl[K, V]) Set(k K, v V) {
+func (t *Cmap[K, V]) Set(k K, v V) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	t.m[k] = v
 }
 
-func (t *cmapImpl[K, V]) SetFunc(k K, f func(V, bool) V) {
+func (t *Cmap[K, V]) SetFunc(k K, f func(V, bool) V) {
 	val, ok := t.Get(k)
 	t.Set(k, f(val, ok))
 }
 
-func (t *cmapImpl[K, V]) Remove(k K) {
+func (t *Cmap[K, V]) Remove(k K) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	delete(t.m, k)
 }
 
-func (t *cmapImpl[K, V]) Get(k K) (v V, ok bool) {
+func (t *Cmap[K, V]) Get(k K) (v V, ok bool) {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	if val, ok := t.m[k]; ok {
@@ -46,7 +33,7 @@ func (t *cmapImpl[K, V]) Get(k K) (v V, ok bool) {
 	return v, false
 }
 
-func (t *cmapImpl[K, V]) GetDefault(k K, def V) V {
+func (t *Cmap[K, V]) GetDefault(k K, def V) V {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	if val, ok := t.m[k]; ok {
@@ -55,14 +42,14 @@ func (t *cmapImpl[K, V]) GetDefault(k K, def V) V {
 	return def
 }
 
-func (t *cmapImpl[K, V]) Has(k K) bool {
+func (t *Cmap[K, V]) Has(k K) bool {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
 	_, ok := t.m[k]
 	return ok
 }
 
-func (t *cmapImpl[K, V]) Keys() <-chan K {
+func (t *Cmap[K, V]) Keys() <-chan K {
 	c := make(chan K)
 	go func() {
 		t.mu.RLock()
@@ -75,7 +62,7 @@ func (t *cmapImpl[K, V]) Keys() <-chan K {
 	return c
 }
 
-func (t *cmapImpl[K, V]) Values() <-chan V {
+func (t *Cmap[K, V]) Values() <-chan V {
 	c := make(chan V)
 	go func() {
 		t.mu.RLock()
@@ -88,19 +75,19 @@ func (t *cmapImpl[K, V]) Values() <-chan V {
 	return c
 }
 
-func (t *cmapImpl[K, V]) Range() <-chan CmapField[K, V] {
+func (t *Cmap[K, V]) Range() <-chan CmapField[K, V] {
 	c := make(chan CmapField[K, V])
 	go func() {
 		t.mu.RLock()
 		defer t.mu.RUnlock()
 		for k, v := range t.m {
-			c <- &cmapFieldImpl[K, V]{k, v}
+			c <- CmapField[K, V]{k, v}
 		}
 		close(c)
 	}()
 	return c
 }
 
-func New[K comparable, V any]() Cmap[K, V] {
-	return &cmapImpl[K, V]{m: make(map[K]V)}
+func New[K comparable, V any]() *Cmap[K, V] {
+	return &Cmap[K, V]{m: make(map[K]V)}
 }
--- cmap/cmap_field.go
+++ cmap/cmap_field.go
@@ -1,19 +1,14 @@
 package cmap
 
-type CmapField[K comparable, V any] interface {
-	Key() K
-	Value() V
-}
-
-type cmapFieldImpl[K comparable, V any] struct {
+type CmapField[K comparable, V any] struct {
 	k K
 	v V
 }
 
-func (t *cmapFieldImpl[K, V]) Key() K {
+func (t *CmapField[K, V]) Key() K {
 	return t.k
 }
 
-func (t *cmapFieldImpl[K, V]) Value() V {
+func (t *CmapField[K, V]) Value() V {
 	return t.v
 }
